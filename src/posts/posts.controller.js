ed6ae4c0078271fa725de191ca475e9ac951dcf6
Phil Willis <philopian@gmail.com>
2023-04-05 23:18:28 -0700

    protect the post routes

--- src/posts/posts.controller.js
+++ src/posts/posts.controller.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const passport = require("passport");
+
 const router = express.Router();
 
 const { getAllItems, getItemById, createItem, updateItem, deleteItem } = require("./posts.service");
 
 // GET all items
-router.get("/", async (req, res) => {
+router.get("/", passport.authenticate("jwt", { session: false }), async (req, res) => {
   // Code to get all items from the database
   try {
     const data = await getAllItems();
@@ -16,7 +18,7 @@ router.get("/", async (req, res) => {
 });
 
 // GET a single item by ID
-router.get("/:id", async (req, res) => {
+router.get("/:id", passport.authenticate("jwt", { session: false }), async (req, res) => {
   // Code to get a single item by ID from the database
   const { id } = req.params;
   try {
@@ -29,7 +31,7 @@ router.get("/:id", async (req, res) => {
 });
 
 // POST a new item
-router.post("/", async (req, res) => {
+router.post("/", passport.authenticate("jwt", { session: false }), async (req, res) => {
   // Code to add a new item to the database
   const { body } = req;
   try {
@@ -42,7 +44,7 @@ router.post("/", async (req, res) => {
 });
 
 // PUT update an existing item by ID
-router.put("/:id", async (req, res) => {
+router.put("/:id", passport.authenticate("jwt", { session: false }), async (req, res) => {
   // Code to update an existing item by ID in the database
   const { id } = req.params;
   const { body } = req;
@@ -56,7 +58,7 @@ router.put("/:id", async (req, res) => {
 });
 
 // DELETE an item by ID
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", passport.authenticate("jwt", { session: false }), async (req, res) => {
   // Code to delete an item by ID from the database
   const { id } = req.params;
   try {
